aa2478f602a3915cd3cf3c00d76acf0fe6af9bec
Dhiraj kumar <126057233+CodeByDhiraj@users.noreply.github.com>
2025-07-06 16:38:18 +0530

    axiosInstance.ts

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "https://netfrix.fallmodz.in",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://netfrix.fallmodz.in",
   headers: {
     "Content-Type": "application/json",
   },
